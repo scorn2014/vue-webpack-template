3db5a60b3e07a6f1a05c8fead9f9759427a98321
scorn2014 <tftrack@163.com>
2019-12-30 22:50:17 +0800

    webpack

--- vue-webpack-template/build/webpack.config.js
+++ vue-webpack-template/build/webpack.config.js
@@ -1,12 +1,78 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    mode: 'production',
-    entry: '../src/main.js',
-    output: {
-      path: path.resolve(__dirname,'../dist'),
-      filename: 'js/[name].[hash:8].js',
-      chunkFilename: 'js/[name].[hash:8].js',
-      publicPath:'./'
-    }
-  };
+  mode: 'production',
+  entry: '../src/main.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].[hash:8].js',
+    chunkFilename: 'js/[name].[hash:8].js',
+    publicPath: './'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/, //匹配
+        exclude: /node_modules/, //排除
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                useBuiltIns: "usage"
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // 将 JS 字符串生成为 style 节点
+        }, {
+          loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+        }, {
+          loader: "sass-loader" // 将 Sass 编译成 CSS
+        }, {
+          loader: 'postcss-loader'
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 3000
+  },
+};
